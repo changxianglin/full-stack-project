a435388e792b0ab9a12ccebd425cbb7e7e16f199
changxianglin <281532612@qq.com>
2026-04-09 20:45:22 +0800

    use router
    include get and post methods

---
+++ blog-express/routes/user.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+router.post('/login', function(req, res, next) {
+    const { username, password } = req.body
+    res.json({
+        username,
+        password,
+    })
+});
+
+module.exports = router;
