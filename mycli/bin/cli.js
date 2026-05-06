6b976bb97e3fe75895c24324f36ffc2fb4b0f779
changxianglin <281532612@qq.com>
2026-05-07 01:04:36 +0800

    add commander
    use commander display command

--- mycli/bin/cli.js
+++ mycli/bin/cli.js
@@ -1,7 +1,9 @@
 #! /usr/bin/env node
-
+const { program } = require('commander')
 // console.log('mycli')
 
-if(process.argv[2] == '--help') {
-    console.log('get cli arg')
-}
+// if(process.argv[2] == '--help') {
+//     console.log('get cli arg')
+// }
+program.option('-f --framwork <framwork>', 'set framwork')
+program.parse(process.argv)
