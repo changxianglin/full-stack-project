7414b37f7ad149485ac90091f399b6658610ec6a
changxianglin <281532612@qq.com>
2026-05-11 19:15:55 +0800

    use inquirer
    write user q and s

--- mycli/lib/core/action.js
+++ mycli/lib/core/action.js
@@ -1,6 +1,19 @@
+var inquirer = require('inquirer')
+
 const myAction = (project, args) => {
-    console.log(project)
-    console.log(args)
+    // console.log(project)
+    // console.log(args)
+
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'framwork',
+            choices: ['express', 'koa', 'egg'],
+            message: 'plase select use framwork'
+        }
+    ]).then(answer => {
+        console.log(answer)
+    })
 }
 
 module.exports = myAction
